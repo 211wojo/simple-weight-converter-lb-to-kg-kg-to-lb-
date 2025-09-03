9ca7e11f58259498204c31320e4b0a406d315e7b
Wojtek <163926800+211wojo@users.noreply.github.com>
2025-09-03 18:33:38 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+weight = float(input("Wprowadz wage: "))
+unit = input("kg/lb?: ")
+
+
+if unit == "kg":
+    typ = "kg"
+    typ2 = "lb"
+    result= weight * 2.2
+    
+elif unit == "lb":
+    typ = "lb"
+    typ2 = "kg"
+    result = weight / 2.2
+
+print(weight, typ, "to", round(result), typ2)
+
